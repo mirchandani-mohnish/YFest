f05c9f3930b760b5f0889db071ea1abff593d6f7
mirchandani-mohnish <mirchandani.mohnish03@gmail.com>
2022-01-20 17:26:20 +0530

    Basic React structure

---
+++ frontend/src/components/ClubAdmin.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ClubAdmin extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default ClubAdmin;
---
+++ frontend/src/components/Clubs.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class Clubs extends Component {
+  render() {
+    return <div></div>;
+  }
+}
