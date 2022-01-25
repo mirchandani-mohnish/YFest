79d44eac81514b0ed51411b0b935afac31001677
mirchandani-mohnish <mirchandani.mohnish03@gmail.com>
2022-01-25 22:03:14 +0530

    routes set

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -21,160 +21,108 @@ export default class Navbar extends Component {
     
 
     return(
-      <div>
-        <div>
-          <nav className="bg-gray-800">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-              <div className="flex items-center justify-between h-16">
-                <div className="flex items-center">
-                  <div className="flex-shrink-0">
-                    <img
-                      className="h-8 w-8"
-                      src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-                      alt="Workflow"
-                    />
-                  </div>
-                  <div className="hidden md:block">
-                    <div className="ml-10 flex items-baseline space-x-4">
-                      <a
-                        href="#"
-                        className=" hover:bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium"
-                      >
-                        Dashboard
-                      </a>
-
-                      <a
-                        href="#"
-                        className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                      >
-                        Team
-                      </a>
-
-                      <a
-                        href="#"
-                        className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                      >
-                        Projects
-                      </a>
-
-                      <a
-                        href="#"
-                        className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                      >
-                        Calendar
-                      </a>
-
-                      <a
-                        href="#"
-                        className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                      >
-                        Reports
-                      </a>
-                    </div>
-                  </div>
-                </div>
-                <div className="-mr-2 flex md:hidden">
-                  <button
-                    onClick={() => this.setIsOpen()}
-                    type="button"
-                    className="bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                    aria-controls="mobile-menu"
-                    aria-expanded="false"
-                  >
-                    <span className="sr-only">Open main menu</span>
-                    {!this.state.isOpen ? (
-                      <svg
-                        className="block h-6 w-6"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        aria-hidden="true"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M4 6h16M4 12h16M4 18h16"
-                        />
-                      </svg>
-                    ) : (
-                      <svg
-                        className="block h-6 w-6"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        aria-hidden="true"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M6 18L18 6M6 6l12 12"
-                        />
-                      </svg>
-                    )}
-                  </button>
-                </div>
-              </div>
-            </div>
-
-            {/* <Transition
-              show={isOpen}
-              enter="transition ease-out duration-100 transform"
-              enterFrom="opacity-0 scale-95"
-              enterTo="opacity-100 scale-100"
-              leave="transition ease-in duration-75 transform"
-              leaveFrom="opacity-100 scale-100"
-              leaveTo="opacity-0 scale-95"
-            >
-              {(ref) => (
-                <div className="md:hidden" id="mobile-menu">
-                  <div ref={ref} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                    <a
-                      href="#"
-                      className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium"
-                    >
-                      Dashboard
-                    </a>
-
-                    <a
-                      href="#"
-                      className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-                    >
-                      Team
-                    </a>
-
-                    <a
-                      href="#"
-                      className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-                    >
-                      Projects
-                    </a>
-
-                    <a
-                      href="#"
-                      className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-                    >
-                      Calendar
-                    </a>
-
-                    <a
-                      href="#"
-                      className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-                    >
-                      Reports
-                    </a>
-                  </div>
-                </div>
-              )}
-            </Transition> */}
-          </nav>
-
+      
+        
+        <div className="h-screen fixed left-0">
           
+          <aside className="flex flex-col items-center bg-white text-gray-700 shadow h-full">
+            
+            <div className="h-16 flex items-center w-full">
+              
+              <a className="h-6 w-6 mx-auto" href="/">
+                <img className="h-6 w-6 mx-auto" src="" alt="YFest" />
+              </a>
+            </div>
+            <ul>
+              {/* Items Section */}
+              <li className="hover:bg-gray-100">
+                <a href="/clubs" className="h-16 px-6 flex flex justify-center items-center w-full
+                  focus:text-orange-500">
+                  <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round">
+                    <polyline points="22 12 16 12 14 15 10 15 8 12 2 12" />
+                    <path d="M5.45 5.11L2 12v6a2 2 0 0 0 2 2h16a2 2 0 0 0
+                      2-2v-6l-3.45-6.89A2 2 0 0 0 16.76 4H7.24a2 2 0 0
+                      0-1.79 1.11z" />
+                  </svg>
+                </a>
+              </li>
+              <li className="hover:bg-gray-100">
+                <a href="/events" className="h-16 px-6 flex flex justify-center items-center w-full
+                  focus:text-orange-500">
+                  <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round">
+                    <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z" />
+                  </svg>
+                </a>
+              </li>
+              <li className="hover:bg-gray-100">
+                <a href="/archives" className="h-16 px-6 flex flex justify-center items-center w-full
+                  focus:text-orange-500">
+                  <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round">
+                    <polyline points="3 6 5 6 21 6" />
+                    <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2
+                      0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
+                  </svg>
+                </a>
+              </li>
+              <li className="hover:bg-gray-100">
+                <a href="/mydashboard" className="h-16 px-6 flex flex justify-center items-center w-full
+                  focus:text-orange-500">
+                  <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round">
+                    <circle cx={9} cy={21} r={1} />
+                    <circle cx={20} cy={21} r={1} />
+                    <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0
+                      2-1.61L23 6H6" />
+                  </svg>
+                </a>
+              </li>
+              <li className="hover:bg-gray-100">
+                <a href="." className="h-16 px-6 flex flex justify-center items-center w-full
+                  focus:text-orange-500">
+                  <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round">
+                    <circle cx={12} cy={12} r={3} />
+                    <path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1
+                      0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0
+                      0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2
+                      2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0
+                      0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1
+                      0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0
+                      0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65
+                      0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0
+                      1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0
+                      1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2
+                      0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0
+                      1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0
+                      2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0
+                      0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65
+                      1.65 0 0 0-1.51 1z" />
+                  </svg>
+                </a>
+              </li>
+              <li className="hover:bg-gray-100">
+                <a href="." className="h-16 px-6 flex flex justify-center items-center w-full
+                  focus:text-orange-500">
+                  <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round">
+                    <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9" />
+                    <path d="M13.73 21a2 2 0 0 1-3.46 0" />
+                  </svg>
+                </a>
+              </li>
+            </ul>
+            <div className="mt-auto h-16 flex items-center w-full">
+              {/* Action Section */}
+              <button className="h-16 w-10 mx-auto flex flex justify-center items-center
+                w-full focus:text-orange-500 hover:bg-red-200 focus:outline-none">
+                <svg className="h-5 w-5 text-red-700" xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round">
+                  <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" />
+                  <polyline points="16 17 21 12 16 7" />
+                  <line x1={21} y1={12} x2={9} y2={12} />
+                </svg>
+              </button>
+            </div>
+          </aside>
         </div>
-      </div>
+
+      
     );
   }
 }
