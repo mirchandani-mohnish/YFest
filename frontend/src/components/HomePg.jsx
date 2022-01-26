c5450d2fd13774e640a1ed364b34984ac7a9f31b
mirchandani-mohnish <mirchandani.mohnish03@gmail.com>
2022-01-26 12:29:59 +0530

    Tailwind components added

--- frontend/src/components/HomePg.jsx
+++ frontend/src/components/HomePg.jsx
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
+import './components.css'
+import 'tw-elements';
 
 export default class HomePg extends Component {
   render() {
     return (
     <div>
-      <section className="h-screen">
-        <section className="w-full h-screen bg-red-200 snap-start">
-          Yfest
+      <section className="h-screen" id="HomeSection">
+        <section className="w-full h-screen">
+          
+
         </section>
-        <section className="w-full h-screen bg-blue-200 snap-start">
+        <section className="w-full h-screen bg-blue-200 ">
           Hi
         </section>
         <section className="w-full h-screen bg-green-200 snap-start">
