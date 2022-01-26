526cd6f452834c2c00eb557a6d7a90ad539e5b11
kavania2002 <kavan155gondalia@gmail.com>
2022-01-26 13:24:46 +0530

    fix/sending statuscode

--- backend/routes/clubs.js
+++ backend/routes/clubs.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+var passport = require("passport");
 // const bodyParser = require('body-parser');
 // app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 
@@ -7,21 +8,29 @@ var router = express.Router();
 const Club = require("../models/club");
 
 router.post("/add", (req, res) => {
-    const clubName = req.body.clubName;
-    const clubDescription = req.body.clubDescription;
+    if (req.isAuthenticated()) {
+        const clubName = req.body.clubName;
+        const clubDescription = req.body.clubDescription;
 
-    console.log(clubName, clubDescription);
+        console.log(clubName, clubDescription);
 
-    const newClub = new Club({
-        clubName: clubName,
-        clubDescription: clubDescription
-    });
+        const newClub = new Club({
+            clubName: clubName,
+            clubDescription: clubDescription
+        });
 
-    console.log(newClub);
-    newClub.save(err => {
-        if (err) console.log(err);
-        res.send(newClub)
-    });
+        console.log(newClub);
+        newClub.save(err => {
+            if (err) {
+                res.send(err);
+            } else {
+                res.send(newClub)
+            }
+        });
+    } else {
+        res.statusCode = 401;
+        res.send("401");
+    }
 })
 
 router.get("/get", (req, res) => {
--- backend/routes/events.js
+++ backend/routes/events.js
@@ -1,34 +1,44 @@
 var express = require("express");
 var router = express.Router();
+var passport = require("passport");
 
 const Event = require("../models/event");
 const Club = require("../models/club");
 
 router.post("/add", (req, res) => {
-    const eventName = req.body.eventName;
-    const clubName = req.body.clubName;
-    const eventDescription = req.body.eventDescription;
+    if (req.isAuthenticated()) {
+        const eventName = req.body.eventName;
+        const clubName = req.body.clubName;
+        const eventDescription = req.body.eventDescription;
 
-    let newEvent = new Event({
-        eventName: eventName,
-        eventDescription: eventDescription,
-        clubName: clubName 
-    })
-    console.log(newEvent);
+        let newEvent = new Event({
+            eventName: eventName,
+            eventDescription: eventDescription,
+            clubName: clubName
+        })
+        console.log(newEvent);
 
-    newEvent.save((err) => {
-        if (err) console.log(err);
+        newEvent.save((err) => {
+            if (err) console.log(err);
 
-        Club.findOne({"clubName" : clubName}, function(err, club){
-            if (club) {
-                club.events.push(newEvent);
-                club.save((err) => {
-                    if (err) console.log(err);
-                    res.send([club, newEvent]);
-                });
-            }
-        });
-    })
+            Club.findOne({ "clubName": clubName }, function (err, club) {
+                if (club) {
+                    club.events.push(newEvent);
+                    club.save((err) => {
+                        if (err) {
+                            res.send(err);
+                        } else {
+                            res.send("Success");
+                        }
+                    });
+                }
+            });
+        })
+    } else {
+        res.statusCode = 401
+        res.redirect("401");
+    }
 });
+
 
 module.exports = router;
