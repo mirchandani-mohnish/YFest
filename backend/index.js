60d497e36639923e749b40d24a7b518d8bb4f7dd
kavania2002 <kavan155gondalia@gmail.com>
2022-01-29 22:51:04 +0530

    add/cookie-parser package

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors({
-  origin: 'http://localhost:8000'
+  origin: 'http://localhost:3000'
 }));
 
 const session1 = {
